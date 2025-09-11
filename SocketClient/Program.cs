446d54e044497cd2f3e2dd7ddd8febb800b1f66d
刘光胜 <liuguangsheng@thape.com.cn>
2025-09-11 17:53:20 +0800

    commit some codes

---
+++ SocketClient/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            IPHostEntry ipHost = Dns.GetHostEntry("127.0.0.1");
+            IPAddress ipAddress = ipHost.AddressList[0];
+
+            IPEndPoint iPEndPoint  = new IPEndPoint(ipAddress, 2112);
+            Console.WriteLine("Starting: Create socket object");
+
+            Socket sender = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            sender.Connect(iPEndPoint);
+
+            Console.WriteLine($"Successfully connected to {sender.RemoteEndPoint}");
+            
+            StringBuilder sb = new StringBuilder();
+            int count = 5000;
+            for(int i=1;i<= count; i++)
+            {
+                sb.Append(i);
+                if(i< count)
+                {
+                    sb.Append(',');
+                }
+                else
+                {
+                    sb.Append('.');
+                }
+            }
+            byte[] forwardMessage = Encoding.ASCII.GetBytes(sb.ToString());
+            sender.Send(forwardMessage);
+
+            byte[] receivedBytes = new byte[1024];
+            int totalBytesReceived = sender.Receive(receivedBytes);
+            Console.WriteLine($"Message provided from server:{Encoding.ASCII.GetString(receivedBytes,0, totalBytesReceived)}");
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+            Console.ReadLine();
+        }
+    }
+}
